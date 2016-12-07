f786bd616120e3a341cd50c8e57c3c5fcb7bd802
farlistener <contact@levieuxcedric.com>
2016-12-07 19:02:34 +0100

    upgrade

--- config/database.php
+++ config/database.php
@@ -1,20 +1,20 @@
 <?php /*
-	Copyright 2014 Cédric Levieux, Jérémy Collot, ArmagNet
+	Copyright 2015 Cédric Levieux, Parti Pirate
 
-	This file is part of OpenTweetBar.
+	This file is part of PPMoney.
 
-    OpenTweetBar is free software: you can redistribute it and/or modify
+    PPMoney is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    OpenTweetBar is distributed in the hope that it will be useful,
+    PPMoney is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with OpenTweetBar.  If not, see <http://www.gnu.org/licenses/>.
+    along with PPMoney.  If not, see <http://www.gnu.org/licenses/>.
 */
 @include_once("config/config.php");
 @include_once("config/salt.php");
@@ -30,7 +30,14 @@ function openConnection() {
 
 	$user = $config["database"]["login"];
 	$password = $config["database"]["password"];
-	$pdo = new PDO($dns, $user, $password );
+
+	$pdo = null;
+	try {
+		$pdo = new PDO($dns, $user, $password );
+	}
+	catch(Exception $e){
+		echo 'Erreur de requète : ', $e->getMessage();
+	}
 
 	return $pdo;
 }
